4397bd71db7af35c1c3b67426998e5e14b88a3cc
mayriv <mayriv@i.ua>
2015-09-06 17:37:14 +0300

    maska9 with auth and skeleton of insertion.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	require("viewer.inc");
+	require("DBManager.inc");
+	require("System.inc");
+	$host = "localhost";
+	$dbName = "maska9";
+	$user = "root";
+	$password = "";
+	DBManager::getInstance()->connect($host, $dbName, $user, $password);
+
+	
+	if ($_GET['action'] == 'getTeams')
+		System::getInstance()->getTeams();
+	else if ($_GET['action'] == 'getGames')
+		System::getInstance()->getGames();
+	else if ($_GET['action'] == 'insertGame')
+		System::getInstance()->insertGame();
+	else 
+		System::getInstance()->getPlayers();
+?>
