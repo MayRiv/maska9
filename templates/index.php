505581336d0c06d9a88813778f752c4680b3f865
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-09-14 13:17:21 +0300

    Design attempts.

---
+++ templates/index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="maska9.css"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+</head>
+<body>
+</head>
+<body>
+	<div id="css-menu-wrapper">
+		<div id="css-menu">
+		<ul>
+			<li><a href="#">
+				<span class="text-top">Игроки</span>
+				<span class="text-bottom">Личный рейтинг</span>
+			</a></li>
+			<li><a href="#">
+				<span class="text-top">Команды</span>
+				<span class="text-bottom">Командный рейтинг</span>
+			</a></li>
+			<li><a href="#">
+				<span class="text-top">Игры</span>
+				<span class="text-bottom">Игровая статистика</span>
+			</a></li>
+		</ul>
+		</div>
+	</div>
+		<table class="table_dark">
+			<tr>
+			    <th>Место</th>
+	    		<th>Игрок</th>
+			    <th>Личный рейтинг</th>
+			    <th>Команда</th>
+	    	</tr>
+	    	<tr>
+		    	<td>1</td>
+		    	<td>Май</td>
+			    <td>30.5</td>
+			    <td>qwerty</td>
+	    	</tr>
+		</table>
+</body>
+</html>
+
